aad536b03d8ce58c13b49a9d52a03788be4c778c
jestin <jestin.jois@xtraininglive.com>
2022-11-03 21:03:19 +0530

    upadated with logger

--- src/provider/constants.ts
+++ src/provider/constants.ts
@@ -1,2 +1,2 @@
-//export  const WS_URL = 'ws://localhost:8000/wss/v2';
-export const WS_URL = 'wss://saasai.xtravision.ai/wss/v2';
+export  const WS_URL = 'ws://localhost:8000/wss/v2';
+// export const WS_URL = 'wss://saasai.xtravision.ai/wss/v2';
