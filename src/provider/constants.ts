56045af0f7d8396a93eeb8316d8d41f0fd4c3966
jestin <jestin.jois@xtraininglive.com>
2023-06-05 09:09:35 +0530

    changed server url

--- src/provider/constants.ts
+++ src/provider/constants.ts
@@ -1,13 +1,13 @@
-export const WS_URL = 'ws://localhost:8000/wss/v2';
+// export const WS_URL = 'ws://localhost:8000/wss/v2';
 
 // production url
-// let serverUrl = 'wss://saasai.xtravision.ai/wss/v2';
-// // tODO: create XTRA_ENV instead od ENV
-// if (process.env.REACT_APP_ENV && process.env.REACT_APP_ENV == 'development') {
-//     serverUrl = 'ws://localhost:8000/wss/v2';
-// }
+let serverUrl = 'wss://saasai.xtravision.ai/wss/v2';
+// tODO: create XTRA_ENV instead od ENV
+if (process.env.REACT_APP_ENV && process.env.REACT_APP_ENV == 'development') {
+    serverUrl = 'ws://localhost:8000/wss/v2';
+}
 
-// export const WS_URL = serverUrl;
+export const WS_URL = serverUrl;
 
 
 // local
