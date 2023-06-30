f19ce03a65b7583f3d1add31f9a130b546418290
Gandhar P <gandhar.pednekar15@gmail.com>
2023-06-30 21:34:26 +0530

    build error fixed for dotenv package

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import json from '@rollup/plugin-json';
 
 const packageJson = require('./package.json');
 
@@ -23,6 +24,7 @@ export default [
       },
     ],
     plugins: [
+      json(),
       peerDepsExternal(),
       resolve(),
       commonjs(),
